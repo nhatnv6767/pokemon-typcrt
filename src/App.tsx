64f13fc93f4898853f20adb8291b3077c83b6e8c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-10-30 21:42:03 +0900

    mot so vidu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
+type Student = {
+  name: string,
+  age: number,
+};
 
-        </div>
-    );
+let student: Student = {
+  name: "nothing",
+  age: 22,
+}
+
+function App() {
+  return (
+      <div className="App">
+
+      </div>
+  );
 }
 
 export default App;
