dd2f4e4ae19a3719345cddab4afb7ab3da313ffe
nhatnv6767 <nhatnvse90183@gmail.com>
2022-11-03 20:13:25 +0900

    function string

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
 import './App.css';
 
-type Student = {
-    name: string,
-    age: number,
-};
-
-interface Student2 {
-    name: string,
-    age: number,
-}
-
-let student: Student2 = {
-    name: "nothing",
-    age: 22,
+const printSomething = () => {
+    console.log("Hello")
 }
 
 function App() {
