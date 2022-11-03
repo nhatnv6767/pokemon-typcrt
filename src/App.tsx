64fc2df3e3eb31d072127757e139fc93cb234c70
nhatnv6767 <nhatnvse90183@gmail.com>
2022-11-03 20:16:16 +0900

    type of function 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
 
-const printSomething: () => void = () => {
-    console.log("Hello")
+const printSomething: (age: number) => string[] = (age: number) => {
+    let name: string[] = ["name1", "name2"];
+    return name;
 }
 
 function App() {
