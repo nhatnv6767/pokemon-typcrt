f6901dc7c0f0a97bf82c0ae990556d2247c45a14
nhatnv6767 <nhatnvse90183@gmail.com>
2022-11-07 21:12:41 +0900

    nothing here

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,6 @@
 import React from 'react';
 import './App.css';
 
-type Name = {
-    name: string
-}
-
-type StudentDetail = Name & {
-    age: number,
-    address: string
-}
-let student: Name = {
-    name: "Not"
-}
-
-let student2: StudentDetail = {
-    name: "NONNN",
-    age: 20,
-    address: "1111"
-}
 
 function App() {
     return (
