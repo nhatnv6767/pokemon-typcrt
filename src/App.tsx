1a726991acda5d9be895b5c0cc5aa202bc6e6ffc
nhatnv6767 <nhatnvse90183@gmail.com>
2022-11-07 21:22:21 +0900

    interface 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import './App.css';
 
 
+interface Name2 {
+    name: string;
+}
+
+interface StudentDetail2 {
+    age: number,
+    address: string,
+}
+
 function App() {
     return (
         <div className="App">
