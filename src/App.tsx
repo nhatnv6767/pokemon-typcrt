a10bc8b45b811d33bf892710eb5116fbe1e14f05
nhatnv6767 <nhatnvse90183@gmail.com>
2022-11-08 20:01:49 +0900

    get api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
 
 // functional component
 const App: React.FC = () => {
+    const [pokemons, setPokemons] = useState<string[]>([]);
+    useEffect(() => {
+        const getPokemon = async () => {
+            const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=20")
+            console.log(res.data)
+        }
+    }, [])
+
     return (
         <div className="App">
             <div className="container">
