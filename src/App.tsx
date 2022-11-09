8f8fe210087cb156f3c3ec8be610a7f8e4fc03e1
nhatnv6767 <nhatnvse90183@gmail.com>
2022-11-09 21:45:33 +0900

    get api by using typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import axios from "axios";
 
+
+interface Pokemons {
+    name: string;
+    url: string;
+}
+
 // functional component
 const App: React.FC = () => {
     const [pokemons, setPokemons] = useState<string[]>([]);
     useEffect(() => {
         const getPokemon = async () => {
             const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=20")
-            res.data.results.forEach(async (pokemon: string) => {
+            res.data.results.forEach(async (pokemon: Pokemons) => {
                 const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
             })
         }
