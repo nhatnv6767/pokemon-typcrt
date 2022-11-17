c780556384760d086d136d617c9d713b4c26319d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-11-17 21:16:47 +0900

    pokemon list 3

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
-const PokemonList = (props) => {
+interface Props {
+    name: string,
+    id: number,
+    image: string,
+}
+
+const PokemonList: React.FC<Props> = (props) => {
+
     return (
         <div>
 
